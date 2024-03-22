4b97f76e6b7dbcea94ee0b1b1da76b53e00266e2
Thomas Stearns <thomas2001.stearns@gmail.com>
2024-03-21 20:36:53 -0700

    implemented like half of the UDP option

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import argparse
 import sys
+import time
 
 parser = argparse.ArgumentParser(
     prog='ChatClient',
@@ -12,72 +13,200 @@ parser.add_argument('--server')
 args = parser.parse_args()
 
 if 65535 < int(args.port) <= 1024:
-    print("Error: Port number out of bounds")
+    print("Error: Port out of bounds")
     exit()
 
 server_ip, server_port = args.server.split(':')
 hostname = socket.gethostname()
 client_ip = socket.gethostbyname(hostname)
+server_address = (server_ip, int(server_port))
+client_address = (client_ip, int(args.port))
 
 global chat_target 
 chat_target = []
 
-def get_input(): # Terminal input loop
+def probe():
+    data = "PROBE\r\n\r\n"
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect(server_address)
+        client_socket.send(data.encode())
+        dataFromServer = client_socket.recv(1024)
+        if dataFromServer: 
+            return 0
+    except socket.error:
+        client_socket.close()
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(client_address)
+    sock.settimeout(2)
+    t_end = time.time() + 2
+    while time.time() < t_end:
+        try:
+            sock.sendto(data.encode(), server_address)
+            data_out, address = sock.recvfrom(1024)
+            if data_out is not None:
+                sock.close()
+                return 1
+        except:
+            sock.close()
+            return -1
+    return -1
+
+def get_input_tcp(): # Terminal input loop for TCP server
     has_registered = False
     for line in sys.stdin:
         if line.rstrip() == '/id':
             print(args.id)
         elif line.rstrip() == '/register':
-            register()
-            has_registered = True
+            register_tcp()
         elif line.rstrip() == '/bridge' and has_registered:
-            bridge()
+            bridge_tcp()
         elif line.rstrip() == '/chat' and len(chat_target) > 0:
-            chat()
+            chat_tcp()
+        elif line.rstrip() == '/quit':
+            quit_tcp()
         else:
             print("bad input")
 
-def register(): # Sends client information to Server
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def get_input_udp(): # Terminal input loop for TCP server
+    has_registered = False
+    for line in sys.stdin:
+        if line.rstrip() == '/id':
+            print(args.id)
+        elif line.rstrip() == '/register':
+            register_udp()
+        elif line.rstrip() == '/bridge' and has_registered:
+            bridge_udp()
+        elif line.rstrip() == '/chat' and len(chat_target) > 0:
+            chat_udp()
+        elif line.rstrip() == '/quit':
+            quit_udp()
+        else:
+            print("bad input")
+
+def quit_tcp(): # Sends a quit packet to server over TCP before shutting down
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        clientSocket.connect((server_ip, int(server_port)))
+        client_socket.connect(server_address)
     except socket.error:
         print("Socket Error: failed to connect")
-        #print(f"attempted to connect to {server_ip}:{server_port}")
-        shutdown(clientSocket)
+        shutdown(client_socket)
+    data = f"QUIT\r\nclientID: {args.id}\r\n\r\n"
+    client_socket.send(data.encode())
+    shutdown(client_socket)
+
+def quit_udp(): # Sends a quit packet to server over UDP before shutting down
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = f"QUIT\r\nclientID: {args.id}\r\n\r\n"
+    sock.bind(client_address)
+    sock.settimeout(2)
+    t_end = time.time() + 2
+    while time.time() < t_end:
+        sock.sendto(data.encode(), server_address)
+        data_out, address = sock.recvfrom(1024)
+        if data_out.decode() == f"QUITACK\r\nclientID: {args.id}\r\n\r\n":
+            shutdown(sock)
+    shutdown(sock)
+
+
+def register_tcp(): # Sends client information to Server over TCP connection
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect(server_address)
+    except socket.error:
+        print("Socket Error: failed to connect")
+        shutdown(client_socket)
     data = f"REGISTER\r\nclientID: {args.id}\r\nIP: {client_ip}\r\nPort: {args.port}\r\n\r\n"
-    clientSocket.send(data.encode())
-    dataFromServer = clientSocket.recv(1024)
-    clientSocket.close()
+    client_socket.send(data.encode())
+    dataFromServer = client_socket.recv(1024)
+    client_socket.close()
     return
 
-def bridge(): # Begins bridge with server, if no other chatters online, enter wait, else receive other client info for connection
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def register_udp(): # Sends client information to Server over UDP connection
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = f"REGISTER\r\nclientID: {args.id}\r\nIP: {client_ip}\r\nPort: {args.port}\r\n\r\n"
+    sock.bind(client_address)
+    sock.settimeout(2)
+
+    t_end = time.time() + 5
+    while time.time() < t_end:
+        sock.sendto(data.encode(), server_address)
+        data_out, address = sock.recvfrom(1024)
+        if data_out.decode() == f"REGACK\r\nclientID: {args.id}\r\nIP: {client_ip}\r\nPort: {args.port}\r\n\r\n":
+            sock.close()
+            return
+    print("Error: No response from server")
+    shutdown(sock)
+
+def bridge_tcp(): # Begins bridge with server, if no other chatters online, enter wait, else receive other client info for connection
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        clientSocket.connect((server_ip, int(server_port)))
+        client_socket.connect((server_ip, int(server_port)))
     except socket.error:
         print("Socket Error: failed to connect")
-        shutdown(clientSocket)
+        shutdown(client_socket)
     data = f"BRIDGE\r\nclientID: {args.id}\r\n\r\n"
-    clientSocket.send(data.encode())
-    dataFromServer = clientSocket.recv(1024)
+    client_socket.send(data.encode())
+    dataFromServer = client_socket.recv(1024)
     data_out = dataFromServer.decode()
     print(data_out)
     data_list = data_out.split('\r\n')
-    #print(f"data_list = {data_list}")
     global chat_target
     if data_list[1].split(':')[1] == ' ':
         chat_target = []
-        wait()
+        wait_tcp()
     else:
         chat_target = data_list[1].split(':')[1].lstrip(), data_list[2].split(':')[1].lstrip(), data_list[3].split(':')[1].lstrip()
-    clientSocket.close()
+    client_socket.close()
     return
 
-def wait(): # Sets up client as server and waits for another client to connect
+def bridge_udp(): # same as bridge_tcp() but over UDP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(client_address)
+    sock.settimeout(2)
+    data = f"BRIDGE\r\nclientID: {args.id}\r\n\r\n"
+
+    sock.sendto(data.encode(), server_address)
+    data_out, address = sock.recvfrom(1024)
+    data_text = data_out.decode()
+    print(data_text)
+    data_list = data_text.split('\r\n')
+    global chat_target
+    if data_list[1].split(':')[1] == ' ':
+        chat_target = []
+        wait_udp()
+    else:
+        chat_target = data_list[1].split(':')[1].lstrip(), data_list[2].split(':')[1].lstrip(), data_list[3].split(':')[1].lstrip()
+    sock.close()
+    return
+        
+def wait_udp(): # waits for another client to send a message
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(60)
+    try:
+        sock.bind(client_address)
+    except OSError:
+        print("Error: Address already in use")
+        shutdown()
+    try:
+        chat_packet, chat_address = sock.recvfrom(1024)
+        chat_text = chat_packet.decode()
+        chat_list = chat_text.split('\r\n')
+        if chat_list[0] == 'CHAT':
+            client_id = chat_list[0].lstrip()
+            print(f"incoming chat request from {client_id}")
+    except:
+        shutdown(sock)
+    try:
+        chat_packet, chat_address = sock.recvfrom(1024)
+    except:
+        shutdown(sock)
+
+def wait_tcp(): # Sets up client as server and waits for another client to connect
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        serverSocket.bind((client_ip, int(args.port)))
+        serverSocket.bind(client_address)
     except OSError:
         print("Error: Address already in use")
         shutdown()
@@ -118,17 +247,20 @@ def wait(): # Sets up client as server and waits for another client to connect
     except KeyboardInterrupt:
         shutdown(clientConnected)
 
-def chat(): # begins chat_loop
+def chat_tcp(): # messages server to indicate a chat has begun, begins chat_loop
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect(server_address)
+    data = f"CHAT\r\nclientID1: {args.id}\r\nclientID2: {chat_target[0]}"
+
     clientSocket.connect((chat_target[1], int(chat_target[2])))
     data = f"CHAT\r\nclientID: {args.id}\r\n\r\n\r\n"
     clientSocket.send(data.encode())
     try:
-        chat_loop(clientSocket)
+        chat_loop_tcp(clientSocket)
     except KeyboardInterrupt:
         shutdown(clientSocket)
 
-def chat_loop(clientSocket): # Connect to other client, begin chatting
+def chat_loop_tcp(clientSocket): # Connect to other client, begin chatting
     for line in sys.stdin: # chat loop
         if line.rstrip() == '/quit':
             data = 'QUIT\r\n\r\n'
@@ -154,9 +286,18 @@ def shutdown(socket=None):
     exit()
 
 def main():
-    print(f"{args.id} running on {client_ip}")
-    while True:
-        get_input()
+    protocol = probe()
+    if protocol == 0:
+        print(f"{args.id} running on {client_ip} over TCP")
+        while True:
+            get_input_tcp()
+    elif protocol == 1:
+        print(f"{args.id} running on {client_ip} over UDP")
+        while True:
+            get_input_udp()
+    else:
+        print("Server is not online, shutting down")
+        shutdown()
 
 if __name__ == "__main__":
     try:
--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import socket
 import argparse
-import sys
 
 parser = argparse.ArgumentParser(
     prog='ChatServer',
     description='Server application for chat network'
 )
 parser.add_argument('--port')
+parser.add_argument('-u', '--udp', action = 'store_true')
 args = parser.parse_args()
 port_int = int(args.port)
 
@@ -20,7 +20,7 @@ server_ip = socket.gethostbyname(hostname)
 
 register_dict = {}
 
-def do_server():
+def do_server_tcp(): # setup socket, bind on address, wait for TCP connection and complete server actions
     print(f"Server is listening on {server_ip}:{args.port}")
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -33,22 +33,76 @@ def do_server():
 
     
     while(True):
-        (clientConnected, clientAddress) = serverSocket.accept()
+        (client_connected, client_address) = serverSocket.accept()
         
-        dataFromClient = clientConnected.recv(1024)
+        data = client_connected.recv(1024)
 
-        packet_type = dataFromClient.decode().split('\r\n')[0]
-        if packet_type == 'REGISTER':
-            clientConnected.send(register(dataFromClient).encode())
-            clientConnected.close()
+        packet_type = data.decode().split('\r\n')[0]
+        if packet_type == 'PROBE':
+            probe_ack = "PROBEACK\r\n\r\n"
+            client_connected.send(probe_ack.encode())
+            client_connected.close()
+        elif packet_type == 'REGISTER':
+            client_connected.send(register(data).encode())
+            client_connected.close()
         elif packet_type == 'BRIDGE':
-            clientConnected.send(bridge(dataFromClient).encode())
-            clientConnected.close()
+            client_connected.send(bridge(data).encode())
+            client_connected.close()
+        elif packet_type == 'CHAT':
+            chat(data)
+            client_connected.close()
+        elif packet_type == 'QUIT':
+            cleanup(data)
+            client_connected.close()
         else:
-            print("ERROR: Unrecognized packet type") #TEST TO FIND OUT THE RIGHT RESPONSE
-    
+            print("ERROR: Unrecognized packet type")
     
 
+def do_server_udp(): # setup socket, bind on address, wait for UDP packets and complete "server" actions
+    print(f"Server is listening on {server_ip}:{args.port}")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.bind((server_ip, port_int))
+    except OSError:
+        print("Error: Address already in use")
+        sock.close()
+        exit()
+    
+    while(True):
+        data, client_address = sock.recvfrom(1024)
+
+        packet_type = data.decode().split('\r\n')[0]
+        if packet_type == 'PROBE':
+            probe_ack = "PROBEACK\r\n\r\n"
+            sock.sendto(probe_ack.encode(), client_address)
+        elif packet_type == 'REGISTER':
+            sock.sendto(register(data).encode(), client_address)
+        elif packet_type == 'BRIDGE':
+            sock.sendto(bridge(data).encode(), client_address)
+        elif packet_type == 'CHAT':
+            chat(data)
+        elif packet_type == 'QUIT':
+            sock.sendto(cleanup(data).encode(), client_address)
+        else:
+            print("ERROR: Unrecognized packet type")
+        
+
+def chat(data):
+    data_list = data.decode().split('\r\n')
+    client_id1 = data_list[1].split(' ')[1]
+    client_id2 = data_list[2].split(' ')[1]
+    del register_dict[client_id1]
+    print(f"CLEANUP: removing {client_id1} from register\n")
+    del register_dict[client_id2]
+    print(f"CLEANUP: removing {client_id2} from register\n")
+
+def cleanup(data):
+    data_list = data.decode().split('\r\n')
+    client_id1 = data_list[1].split(' ')[1]
+    if client_id1 in register_dict.keys():
+        del register_dict[client_id1]
+        print(f"CLEANUP: removing {client_id1} from register\n")
+    return f"QUITACK\r\nclientID: {client_id1}\r\n\r\n"
 
 def register(data):
     data_list = data.decode().split('\r\n')
@@ -58,7 +112,7 @@ def register(data):
     register_dict[client_id] = [client_ip, client_port]
     print(f"REGISTER: {client_id} from {client_ip}:{client_port} received")
 
-    data_out = "REGACK\r\nclientID: {client_id}\r\nIP: {client_ip}\r\nPort: {client_port}\r\n\r\n"
+    data_out = f"REGACK\r\nclientID: {client_id}\r\nIP: {client_ip}\r\nPort: {client_port}\r\n\r\n"
     return data_out
 
 def bridge(data):
@@ -74,7 +128,10 @@ def bridge(data):
 
 def main():
     try:
-        do_server()
+        if args.udp:
+            do_server_udp()
+        else:
+            do_server_tcp()
     except KeyboardInterrupt:
         exit()
 
